f22a9390594b409369a5be8d8a3dde1a90a9c73c
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2022-08-08 11:12:44 -0400

    Test: initial setup and test for Item

---
+++ spec/item_spec.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Item do
+  let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+  let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+
+
+  it 'should exist as a class' do
+    expect(item1).to be_a Item
+  end
+
+  it 'should have a name attribute for the instance object' do
+    expect(item2.name).to eq ("Tomato")
+  end
+
+  it 'should return a price for the item' do
+    expect(item2.price).to eq 0.5
+  end
+
+end
